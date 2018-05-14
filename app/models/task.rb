1fb91931e069bf49dd1cb2946a7c34f06ba86ff2
Kurshak <l.tisha@mail.ru>
2018-05-14 12:24:47 +0300

    Ну теперь точно,все работает как часы

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,18 +1,31 @@
 class Task < ApplicationRecord
     belongs_to :user
-    validates :name, :description, :time, presence: true
-   
+    validates :name, :description, presence: true
+    
+   #потому что опасный 
     def complit!      
        self.complited = true 
        save!
     end
 
     def fail!
-            if self.time < Time.now and self.complited == nil
+            if self.time < (Time.zone.now) and self.complited == nil
                 self.complited = false
                 save!
             end
     end   
 
+
+    def gre!
+        if self.time > (Time.zone.now) and self.complited == false
+            self.complited = nil
+            save!
+        end
+    end
+
+
 end
+
+
+
 

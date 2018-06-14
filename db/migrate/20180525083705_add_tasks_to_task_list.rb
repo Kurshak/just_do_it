f6ca54cc44cbbe0766e79db4465e5e445e6a9d99
Kurshak <l.tisha@mail.ru>
2018-06-14 14:33:30 +0300

    косяк

--- db/migrate/20180525083705_add_tasks_to_task_list.rb
+++ db/migrate/20180525083705_add_tasks_to_task_list.rb
@@ -1,6 +1,6 @@
 class AddTask_listToTasks < ActiveRecord::Migration[5.1]
   def change
     add_column :tasks, :task_list_id, :integer
-    add_index :tasks :task_list_id
+    add_index :tasks, :task_list_id
   end
 end
